f8cd8c2d1958d3058c212abb22d305363d0f865f
Omar Moodie <axiom2011@gmail.com>
2021-12-16 18:44:05 -0800

    Added boxplot

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 ''' 
 
@@ -13,14 +14,17 @@ class DataAnalysis:
     ''' Default arguments will show certain things based on provided arguments during class object creation. 
         This allows the user to see what they would like and not even have to manually call the function
         themselves. '''
-    def __init__(self, df, showDataDetails=False, showCountPlot=False):
+    def __init__(self, df, showDataDetails=False, showCountPlot=False, showBoxPlot=False):
         self.m_df = df
 
         if showDataDetails is True:
             self.ShowDataFrameDetails()
 
         if showCountPlot is True:
-            self.ShowCountPlot()
+            self.CountPlot()
+
+        if showBoxPlot is True:
+            self.BoxPlot()
 
 
     def ShowDataFrameDetails(self):
@@ -42,8 +46,24 @@ class DataAnalysis:
 
     ''' View how many types of the target variables there are in graph using seaborn. 
         Also terminal to view it later after user exists graph. '''
-    def ShowCountPlot(self):
+    def CountPlot(self):
         valueCounts = self.m_df['diagnosis'].value_counts()
         print(f'---value_counts of target variable---:\n{valueCounts}')
         sns.countplot(self.m_df['diagnosis'])
+        plt.show()
+
+
+    ''' Box plots are a bit useful. They demonstrate the distribution of data. Using a
+        minimum, q1 (quartile), median, q3, and maximum, the box plot has the ability
+        to tell us about outliers. If outliers will be significant in the project, this
+        will help identify them. Also determines if the data is symmetrical of not as an
+        imbalance could be important to see.
+        
+        Ex: The outliers will be displayed by little diamonds in the boxplot in the
+            function. For M, the top or highest outlier will be 2501. Seeing the max
+            value in the area_mean feature/column, we can confirm this with the code:
+            "print('Max value is ' + str(np.amax(self.m_df['area_mean'].values)))". The
+            result matches the afforementioned outlier. '''
+    def BoxPlot(self):
+        sns.boxplot(x='diagnosis', y='area_mean', data=self.m_df)
         plt.show()
--- main.py
+++ main.py
@@ -14,30 +14,30 @@ df = pd.read_csv('wisc_bc_data.csv')
 
 
 # --- Data analysis just involves exploring the data for what is has. See data_analysis.py for explanation for last 3 default arguments.
-da = DataAnalysis(df)
+da = DataAnalysis(df, False, False, True)
 
 
-# --- Feature engineering performs operations on features to increase performance. Vital step.
-fe = FeatureEngineering(df)
-fe.EncodeCategoricalColumns()
-fe.UpdateFeaturesForCorrelation()
+# # --- Feature engineering performs operations on features to increase performance. Vital step.
+# fe = FeatureEngineering(df)
+# fe.EncodeCategoricalColumns()
+# fe.UpdateFeaturesForCorrelation()
 
 
-# --- Model training is like letting a student study. Here a part of feature engineering is applied.  Return models as well.
-mt = ModelTraining(df)
-x_train, x_test, y_train, y_test = mt.GetTrainAndTestSets()
-x_train, x_test = fe.UseScaling(x_train, x_test)
-mt.Train(x_train, y_train)
-mt.AdaBoostModel()
-mt.XgBoostModel()
-mt.GradientBoost()
+# # --- Model training is like letting a student study. Here a part of feature engineering is applied.  Return models as well.
+# mt = ModelTraining(df)
+# x_train, x_test, y_train, y_test = mt.GetTrainAndTestSets()
+# x_train, x_test = fe.UseScaling(x_train, x_test)
+# mt.Train(x_train, y_train)
+# mt.AdaBoostModel()
+# mt.XgBoostModel()
+# mt.GradientBoost()
 
 
-# --- Use for accuracy and what not.
-em = EvaluateModels(mt.GetModels(), x_test, y_test)
-em.ViewClassificationReport()
-em.ViewAccuracy()
-em.ViewConfusionMatrices()
-em.CustomModelMetrics()
-em.PlotRocCurves()
-em.ApplyRegularization(x_train, y_train)
+# # --- Use for accuracy and what not.
+# em = EvaluateModels(mt.GetModels(), x_test, y_test)
+# em.ViewClassificationReport()
+# em.ViewAccuracy()
+# em.ViewConfusionMatrices()
+# em.CustomModelMetrics()
+# em.PlotRocCurves()
+# em.ApplyRegularization(x_train, y_train)
