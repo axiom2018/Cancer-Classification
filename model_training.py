b455a86cf7c5517184e760f601f50aa65a037d2c
Omar Moodie <axiom2011@gmail.com>
2021-12-28 18:17:46 -0800

    Updated VotingClassifier functiom, new implementation makes more sense

--- model_training.py
+++ model_training.py
@@ -1,11 +1,15 @@
+from numpy.core.fromnumeric import var
 import optuna
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import KFold, train_test_split, cross_val_score, cross_validate
+from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import VotingClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
 from sklearn import preprocessing
-from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score
 
 
 ''' 
@@ -71,8 +75,8 @@ class ModelTraining:
                 
         '''
         C = trial.suggest_loguniform("C", 1e-7, 10.0) 
-        solver = trial.suggest_categorical("solver", ("lbfgs", "newton-cg"))
-        max_iter = trial.suggest_int("max_iter", 200, 500)
+        solver = trial.suggest_categorical("solver", ("lbfgs", "saga"))
+        max_iter = trial.suggest_int("max_iter", 4000, 4000)
 
         pipe = make_pipeline(preprocessing.StandardScaler(), 
                 LogisticRegression(C=C, solver=solver, max_iter=max_iter))
@@ -143,10 +147,34 @@ class ModelTraining:
         dt = DecisionTreeClassifier(criterion=criterion, splitter=splitter, max_depth=max_depth, 
             min_samples_split=min_samples_split)
 
-
         score = cross_val_score(dt, self.m_X, self.m_y, n_jobs=1, cv=3)
         accuracy = score.mean()
         return accuracy
+
+
+
+    def Svm(self, trial):
+        C = trial.suggest_loguniform("C", 0.5, 1.0)
+        kernel = trial.suggest_categorical("kernel", {"rbf", "linear", "poly"})
+
+        # Initialize the model, then get the score and accuracy.
+        svc = SVC(C=C, kernel=kernel)
+        svc.fit(self.m_x_train, self.m_y_train)
+
+        score = cross_val_score(svc, self.m_X, self.m_y, n_jobs=1, cv=3)
+        accuracy = score.mean()
+        return accuracy
+
+
+
+    def NaiveBayes(self, trial):
+        var_smoothing = trial.suggest_loguniform("var_smoothing", 1e-2, 10.0)
+
+        nb = GaussianNB(var_smoothing=var_smoothing)
+        nb.fit(self.m_x_train, self.m_y_train)
+        
+        return nb.score(self.m_x_test, self.m_y_test)
+
 
 
 
@@ -231,52 +259,86 @@ class ModelTraining:
 
 
 
+    def ApplySVM(self, showSteps, epochs=100):
+        if showSteps is True:
+            print('Starting svm optimization.')
+
+        study = optuna.create_study(direction='maximize')
+        study.optimize(self.Svm, n_trials=epochs)
+
+        trial = study.best_trial
+
+        C = trial.params["C"]
+        kernel = trial.params["kernel"]
+
+        svc = SVC(C=C, kernel=kernel)
+        svc.fit(self.m_x_train, self.m_y_train)
+
+        if showSteps is True:
+            print('Svm optimized.')
+
+        return svc
+
+
+
+    def ApplyNaiveBayes(self, showSteps, epochs=100):
+        if showSteps is True:
+            print('Starting naive bayes optimization.')
+
+        study = optuna.create_study(direction='maximize')
+        study.optimize(self.NaiveBayes, n_trials=epochs)
+
+        trial = study.best_trial
+
+        var_smoothing = trial.params["var_smoothing"]
+
+        nb = GaussianNB(var_smoothing=var_smoothing)
+        nb.fit(self.m_x_train, self.m_y_train)
+
+        if showSteps is True:
+            print('Naive bayes optimized.')
+
+        return nb
+
+
+
     ''' The main function of this class. In order to get the voting classification
         to fit in, there needs to be multiple different algorithms ready. In order
         to have different ones ready, of course the multiple above functions utilizing
         Optuna must be ran. This function will run them all, get their trained models,
         and then use a voting classification to get the best model. '''
-    def TrainModels(self, showSteps=False):
+    def TrainModelsVotingClassifier(self, showSteps=False):
         if showSteps is True:
             print('---Model training beginning---')
 
-        self.m_models.append(('Logistic Regression', self.ApplyLogisticRegression(True)))
+        self.m_models.append(('Logistic Regression', self.ApplyLogisticRegression(showSteps)))
         self.m_models.append(('Random Forest', self.ApplyRandomForest(showSteps, 50)))
         self.m_models.append(('Decision Tree', self.ApplyDecisionTree(showSteps)))
+        self.m_models.append(('SVM', self.ApplySVM(showSteps, 20)))
+        self.m_models.append(('Naive Bayes', self.ApplyNaiveBayes(showSteps)))
 
         if showSteps is True:
             print('---Model training is done.---\n')
 
         # Initialize voting classifier and fit on x/y train data
         vc = VotingClassifier(estimators=self.m_models, voting='hard')
-        vc.fit(self.m_x_train, self.m_y_train)
-
-        # For a type of model evaluation.
-        typeOfScoring = 'accuracy'
-
-        if showSteps is True:
-            print(f'---Printing {typeOfScoring} scores.---')        
-
-        # Loop through all models, get information and print it. 
-        for model in self.m_models:
-            score = cross_val_score(model[1], X=self.m_x_train, y=self.m_y_train, scoring=typeOfScoring, cv=10).mean()
-            print(f'{model[0]} accuracy score is {score}.')
-
-            ''' Get best model regarding score on every iteration. 
-            
-                If the variable is none, no model has been chosen. So if nothing is able 
-                to be compared to it yet, set whatever model in the loop as the chosen one.  '''
-            if self.m_bestModel is None:
-                if showSteps is True:
-                    print(f'-----Current best model: {model[0]} with score: {score}')
-                self.m_bestModel = (model[0], model[1], score)
-            else:
-                # Is the new model score, better than the current chosen one? If so, update chosen model.
-                if score > self.m_bestModel[2]:
-                    if showSteps is True:
-                        print(f'-----NEW best model: {model[0]} with new score: {score}')
-
-                    self.m_bestModel = (model[0], model[1], score)
-            
         
-        print(f'\nBest model {self.m_bestModel[0]}. Score: {self.m_bestModel[2]}.')
+        ''' Quick cross validation used to see how the models are doing.
+        
+            KFold - Splits data into k amount of folds.
+        
+            Scores - The function cross_validation will return a dictionary and the main
+                key of interest is "test_score" to of course means the scores of every
+                model that was used. Also apply the KFold #. 
+                
+            Then print out the test scores. '''
+        kf = KFold(shuffle=True, random_state=11)
+        scores = cross_validate(vc, X=self.m_x_train, y=self.m_y_train, cv=kf)
+        testScores = scores['test_score']
+        print(f'Cross validation scores:\n{testScores}')
+
+        # Fit then get predictions and finally see the accuracy.
+        vc.fit(self.m_x_train, self.m_y_train)
+        vcPred = vc.predict(self.m_x_test)
+
+        print(f'Accuracy score of voting classifier:\n{accuracy_score(self.m_y_test, vcPred)}\n')
